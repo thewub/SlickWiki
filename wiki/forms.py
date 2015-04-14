3cda242424a81d381298cebc0ccc807e73c15968
thewub <thewub.wiki@googlemail.com>
2015-04-14 21:01:50 +0100

    increase size of editing textarea

--- wiki/forms.py
+++ wiki/forms.py
@@ -6,3 +6,9 @@ class EditForm(forms.ModelForm):
     class Meta:
         model = Revision
         fields = ['text', 'comment']
+        widgets = {
+            'text' : forms.Textarea(attrs={'cols': 40, 'rows': 15})
+        }
+        labels = {
+            'text' : ''
+        }
