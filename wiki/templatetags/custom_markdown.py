7aad1b581c0e0f26764624f797137b1e09b980ef
thewub <thewub.wiki@googlemail.com>
2015-04-14 13:08:19 +0100

    add syntax highlighting (needs Pygments)

--- wiki/templatetags/custom_markdown.py
+++ wiki/templatetags/custom_markdown.py
@@ -3,6 +3,7 @@
 
 import markdown
 from markdown.extensions.toc import TocExtension
+from markdown.extensions.codehilite import CodeHiliteExtension
 
 from django import template
 from django.template.defaultfilters import stringfilter
@@ -14,7 +15,9 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def custom_markdown(value):
-    extensions = ['wikilinks', TocExtension(baselevel=2, title='Contents', permalink=u'#')]
+    extensions = ['wikilinks', 
+                  TocExtension(baselevel=2, title='Contents', permalink=u'#'),
+                  CodeHiliteExtension(guess_lang=True, use_pygments=True)]
     return mark_safe(markdown.markdown(force_unicode(value),
                                         extensions,
                                         safe_mode=True,
