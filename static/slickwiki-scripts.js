7ffa12eb7761556a4a044bf88f6fbbb0f514650a
thewub <thewub.wiki@googlemail.com>
2015-04-26 00:15:04 +0100

    use a javascript solution for anchor targets
    The previous CSS solution to make sure anchor targets weren’t covered
    by the fixed navbar had serious problems. Links in the section above
    often became unclickable.
    
    Unfortunately I couldn’t find a good CSS solution (while keeping the
    .headerlinks clickable too). So settled for some javascript.
    
    Unfortunately this doesn’t seem to trigger the :target selector. Should
    revisit this later to see if I can get that to work again

--- static/slickwiki-scripts.js
+++ static/slickwiki-scripts.js
@@ -20,4 +20,42 @@ $(document).ready(function() {
     });
     /* End of sortable tables */
 
+    // When our page loads, check to see if it contains an anchor
+    scroll_if_anchor(window.location.hash);
+
+    // Intercept all anchor clicks
+    $("body").on("click", "a[href^='#']", scroll_if_anchor);
+
 });
+
+
+/**
+  * From http://stackoverflow.com/a/13067009/3438044
+  * Check an href for an anchor. If exists, and in document, scroll to it.
+  * If href argument omitted, assumes context (this) is HTML Element,
+  * which will be the case when invoked by jQuery after an event
+  */
+function scroll_if_anchor(href) {
+
+    // Offset in px
+    var fromTop = 60;
+
+    href = typeof(href) == "string" ? href : $(this).attr("href");
+
+    // If href missing, ignore
+    if(!href) return;
+
+    // If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
+    // Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+    var $target = $(href);
+
+    // Older browsers without pushState might flicker here, as they momentarily
+    // jump to the wrong position (IE < 10)
+    if($target.length) {
+        $('html, body').animate({ scrollTop: $target.offset().top - fromTop }, 200);
+        if(history && "pushState" in history) {
+            history.pushState({}, document.title, window.location.pathname + href);
+            return false;
+        }
+    }
+}
