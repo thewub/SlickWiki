6893369c058e3064fe8fe0526abb872a7a2c4f99
thewub <thewub.wiki@googlemail.com>
2016-06-27 18:58:50 +0200

    don't pass strings to url()
    deprecated from Django 1.10

--- SlickWiki/urls.py
+++ SlickWiki/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from wiki.models import Article, Revision, User
-
+import wiki.views
+import django.contrib.auth.views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -23,9 +24,9 @@ urlpatterns = patterns('',
             template_name='wiki/recent_changes.html') ),
 
     # User stuff
-    url(r'special/login/?', 'django.contrib.auth.views.login'),
-    url(r'special/logout/?', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-    url(r'special/createaccount/?', 'wiki.views.create_account'),
+    url(r'special/login/?', django.contrib.auth.views.login),
+    url(r'special/logout/?', django.contrib.auth.views.logout, {'next_page': '/'}),
+    url(r'special/createaccount/?', wiki.views.create_account),
 
     url(r'special/userlist/?', 
         ListView.as_view(
@@ -34,16 +35,16 @@ urlpatterns = patterns('',
             template_name='wiki/user_list.html') ),
 
     # User
-    url(r'user/(?P<username>[-a-zA-Z0-9]+)/?$', 'wiki.views.user_info'),
+    url(r'user/(?P<username>[-a-zA-Z0-9]+)/?$', wiki.views.user_info),
 
     # Search articles
-    url(r'special/search/?', 'wiki.views.get_search_results'),
+    url(r'special/search/?', wiki.views.get_search_results),
 
     # Individual revision
     url(r'revision/(?P<pk>\d+)/?$', DetailView.as_view(model=Revision)),
 
     # Diff
-    url(r'special/diff/(?P<rev1id>\d+)/(?P<rev2id>\d+)/?$', 'wiki.views.diff'),
+    url(r'special/diff/(?P<rev1id>\d+)/(?P<rev2id>\d+)/?$', wiki.views.diff),
     
     # Article list
     url(r'^$', 
@@ -52,11 +53,11 @@ urlpatterns = patterns('',
             template_name='wiki/index.html') ),
 
     # Edit article
-    url(r'(?P<slug>[-a-zA-Z0-9\- ]+)/edit/?$', 'wiki.views.edit_article'),
+    url(r'(?P<slug>[-a-zA-Z0-9\- ]+)/edit/?$', wiki.views.edit_article),
 
     # History of article
-    url(r'(?P<slug>[-a-zA-Z0-9\- ]+)/history/?$', 'wiki.views.article_history'),
+    url(r'(?P<slug>[-a-zA-Z0-9\- ]+)/history/?$', wiki.views.article_history),
 
     # View article
-    url(r'^(?P<slug>[-a-zA-Z0-9\- ]+)/?$', 'wiki.views.view_article'),
+    url(r'^(?P<slug>[-a-zA-Z0-9\- ]+)/?$', wiki.views.view_article),
 )
