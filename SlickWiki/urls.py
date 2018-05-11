3599813cb4ac3472dd0e9b68a236de7c11217292
thewub <thewub.wiki@googlemail.com>
2018-05-11 23:00:07 +0100

    update to new 'path' urls
    https://docs.djangoproject.com/en/2.0/releases/2.0/#simplified-url-routi
    ng-syntax

--- SlickWiki/urls.py
+++ SlickWiki/urls.py
@@ -10,25 +10,22 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'SlickWiki.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    re_path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     # Recent changes
-    re_path(r'special/recentchanges/?', 
+    path('special/recentchanges/', 
         ListView.as_view(
             model=Revision,
             paginate_by=20,
             template_name='wiki/recent_changes.html') ),
 
     # User stuff
-    re_path(r'special/login/?', django.contrib.auth.views.login, name='login'),
-    re_path(r'special/logout/?', django.contrib.auth.views.logout, {'next_page': '/'}),
-    re_path(r'special/createaccount/?', wiki.views.create_account),
+    path('special/login/', django.contrib.auth.views.login, name='login'),
+    path('special/logout/', django.contrib.auth.views.logout, {'next_page': '/'}),
+    path('special/createaccount/', wiki.views.create_account),
 
-    re_path(r'special/userlist/?', 
+    path(r'special/userlist/', 
         ListView.as_view(
             model=User,
             ordering=['date_joined'],
@@ -36,27 +33,27 @@ urlpatterns = [
             template_name='wiki/user_list.html') ),
 
     # User
-    re_path(r'user/(?P<username>[-a-zA-Z0-9]+)/?$', wiki.views.user_info),
+    path('user/<username>/', wiki.views.user_info),
 
     # Search articles
-    re_path(r'special/search/?', wiki.views.get_search_results),
+    path('special/search/', wiki.views.get_search_results),
 
     # Individual revision
-    re_path(r'revision/(?P<pk>\d+)/?$', DetailView.as_view(model=Revision)),
+    path('revision/<int:pk>/', DetailView.as_view(model=Revision)),
 
     # Diff
-    re_path(r'special/diff/(?P<rev1id>\d+)/(?P<rev2id>\d+)/?$', wiki.views.diff),
+    path('special/diff/<int:rev1id>/<int:rev2id>/', wiki.views.diff),
     
     # Index
-    re_path(r'^$', ListView.as_view( model=Article, template_name='wiki/index.html', ordering='current_revision' ) ),
+    path('', ListView.as_view( model=Article, template_name='wiki/index.html', ordering='current_revision' ) ),
 
     # Edit article
-    re_path(r'(?P<slug>[-a-zA-Z0-9\- ]+)/edit/?$', wiki.views.edit_article),
+    path('<slug:slug>/edit/', wiki.views.edit_article),
 
     # History of article
-    re_path(r'(?P<slug>[-a-zA-Z0-9\- ]+)/history/?$', wiki.views.article_history),
+    path('<slug:slug>/history/', wiki.views.article_history),
 
     # View article
-    re_path(r'^(?P<slug>[-a-zA-Z0-9\- ]+)/?$', wiki.views.view_article),
+    path('<slug:slug>/', wiki.views.view_article),
 
 ]
